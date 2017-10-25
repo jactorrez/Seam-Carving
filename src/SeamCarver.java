ea5a35ee1d62f498ae9751e4662ab32bf7513354
Javier Torrez <javierctorrez@gmail.com>
2017-10-24 21:25:29 -0400

    refactored and increased efficiency of code by implementing the pixel graph using a 2D array instead of an explicit digraph

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -10,9 +10,7 @@ import structs.graphs.Edge;
 public class SeamCarver {
 	
 	private Picture picture;
-	private Digraph<Integer,Boolean> energyGraph;
-	private HashMap<Pixel, Vertex<Integer>> pixelToVertexMap = new HashMap<>();
-	private HashMap<Vertex<Integer>, Pixel> vertexToPixelMap = new HashMap<>();
+	private int[][] energyGraph, distGraph;
 	
 	private int width;
 	private int height;
@@ -23,47 +21,72 @@ public class SeamCarver {
 			throw new NullPointerException("Null picture given");
 		
 		this.picture = picture;
-		this.width = picture.width()-1;
-		this.height = picture.height()-1;
+		this.width = picture.width();
+		this.height = picture.height();
 		
 		System.out.println("Dimensions: " + height + " x " + width);
 		
-		this.energyGraph = new Digraph<>();
+		this.energyGraph = new int[height][width];
+		this.distGraph = new int[height][width];
 		
-		for(int row = 0; row <= height; row++){
-			for(int col = 0; col <= width; col++){
-				Pixel pixel = new Pixel(col, row);
-			
-				int energy = ((Double)energy(col, row)).intValue();
-				pixelToVertexMap.put(pixel, energyGraph.insertVertex(energy));
-			}
-		}
-		
-		for(int row = 0; row <= height; row++){
-			for(int col = 0; col <= width; col++){
-				Vertex<Integer> current = getVertexByPixel(col, row);
-				//System.out.println("col/row of pixel " + col + " " + row);
-				vertexToPixelMap.put(current, new Pixel(col, row));
-				//System.out.println("returned: " + getPixelByVertex(current).getColumn() + " and " + getPixelByVertex(current).getRow());
-				
-				Vertex<Integer> bottomLeft = (col-1 < 0) ? null : getVertexByPixel(col-1,row+1);
-				Vertex<Integer> bottom = (row + 1 > height)? null : getVertexByPixel(col, row+1);
-				Vertex<Integer> bottomRight = (col + 1 > width) ? null : getVertexByPixel(col+1, row+1);
-				
-				if(bottomLeft != null){
-					energyGraph.insertEdge(current, bottomLeft, true);
-				}
-				
-				if(bottom != null){
-					energyGraph.insertEdge(current, bottom, true);
-				}
-				
-				if(bottomRight != null){
-					energyGraph.insertEdge(current, bottomRight, true);
-				}
-			}
-		}
+		//int energy = ((Double) energy(col, row)).intValue();
+		createEnergyGraph(energyGraph);
+		createDistanceGraph(energyGraph, distGraph);
 	}
+	
+	private int[][] createEnergyGraph(int[][] graph){
+		int height = graph.length;
+		int width = graph[0].length;
+		
+		for(int row = 0; row < height; row++){
+			for(int col = 0; col < width; col++){
+				graph[row][col] = ((Double) energy(col, row)).intValue();
+			}
+		}
+		
+		return graph;
+	}
+	
+	private int[][] createDistanceGraph(int[][] energy, int[][] distTo){
+		int height = distTo.length;
+		int width = distTo[0].length;
+		
+		for(int row = 0; row < height; row++){
+			for(int col = 0; col < width; col++){
+				distTo[row][col] = Integer.MAX_VALUE;
+			}
+		}
+		
+		for(int row = 0; row < height-1; row++){
+			for(int col = 0; col < width; col++){
+				
+				if(row == 0){
+					distTo[0][col] = energyGraph[0][col];
+				}	
+				
+				int sumBy = -1;
+				int u = distTo[row][col];
+				
+				for(int i = 1; i <= 3; i++){
+					int colSum = col + (sumBy++);
+					
+					if((colSum < 0) || (colSum >= width)){
+						continue;
+					} 
+					
+					int newDistance = u + energy[row+1][colSum];
+					int oldDistance = distTo[row+1][colSum];
+				
+					if(newDistance < oldDistance){
+						distTo[row+1][colSum] = newDistance;
+					}
+				}
+			}
+		}
+		
+		return distTo;
+	}
+
 	
 	// Calculate energy of pixel 
 	// The energy of a pixel is the square of the x gradient + the square of the y gradient 
@@ -75,20 +98,6 @@ public class SeamCarver {
 	// To handle pixels on the borders of the image, calculate energy by defining the leftmost and rightmost columns 
 	// as adjacent and the topmost and bottommost rows as adjacent.
 	
-	private Vertex<Integer> getVertexByPixel(int col, int row){
-		if(((col < 0 || col > width)) || ((row < 0 || row > height)))
-			return null;
-		
-		return pixelToVertexMap.get(new Pixel(col, row));
-	}
-	
-	private Pixel getPixelByVertex(Vertex<Integer> v){
-		if(v == null){
-			throw new NullPointerException("Null value given");
-		}
-		
-		return vertexToPixelMap.get(v);
-	}
 	
 	// Current picture
 	public Picture picture(){
@@ -106,27 +115,29 @@ public class SeamCarver {
 	}
 	
 	// Energy of pixel at column x and row y
-	public double energy(int x, int y){
-		if((x < 0 || x > width) || (y < 0 || y > height))
-			throw new IndexOutOfBoundsException("Coordinates must be between pixel dimension bounds");
+	public double energy(int c, int r){
+		int width = this.width-1;
+		int height = this.height-1;
+		if((c < 0 || c > width) || (r < 0 || r > height))
+			throw new IndexOutOfBoundsException("Coordinates must be between image dimension bounds");
 		
-		int col = x;
-		int row = y;
+		int col = c;
+		int row = r;
 		
 		// Calculating the x-gradient 
 		int rightPos = (col + 1 > width) ? 0 : col + 1; 
 		int leftPos = (col - 1 < 0) ? width : col - 1;
 		
-		Color rightNeighbor = new Color(picture.getRGB(rightPos, row));
-		Color leftNeighbor = new Color(picture.getRGB(leftPos, row));
+		Color rightNeighbor = picture.get(rightPos, row);
+		Color leftNeighbor = picture.get(leftPos, row);
 		int xGradient = getXGradient(rightNeighbor, leftNeighbor);
 		
 		// Calculating the y-gradient 
 		int topPos = (row - 1 < 0) ? height : row - 1;
 		int bottomPos = (row + 1 > height) ? 0 : row + 1;
 		
-		Color topNeighbor = new Color(picture.getRGB(col, topPos));
-		Color bottomNeighbor = new Color(picture.getRGB(col, bottomPos));
+		Color topNeighbor = picture.get(col, topPos);
+		Color bottomNeighbor = picture.get(col, bottomPos);
 		int yGradient = getYGradient(topNeighbor, bottomNeighbor);
 		
 		return (xGradient + yGradient);
@@ -137,57 +148,11 @@ public class SeamCarver {
 		int[] seam = new int[width];
 		
 		return new int[]{1,2,3};
-		
 	}
 	
 	// Sequence of indices for vertical seam
 	public int[] findVerticalSeam(){
-		int[] seam = new int[height];
-		int min = Integer.MAX_VALUE;
-		
-		for(int col = 0; col <= width; col++){
-			int currentMin = 0;
-			int currentRow = 0;
-			int[] currentSeam = new int[height];
-			Vertex<Integer> source = getVertexByPixel(col, 0);
-			
-			AdaptableHeap<Integer, Vertex<Integer>> pq = new AdaptableHeap<>();
-			pq.insert(source.getElement(), source);
-			
-			while(!pq.isEmpty()){
-				System.out.println("searching");
-				Entry<Integer, Vertex<Integer>> pqMin = pq.removeMin();
-				Vertex<Integer> currentV = pqMin.getValue(); 
-				Pixel currentPixel = getPixelByVertex(currentV);
-				System.out.println("Current pixel row: " + currentRow);
-				System.out.println(currentPixel.getRow());
-				
-				if(currentPixel.getRow() == height){
-					System.out.println("reached height of " + height + " at row " + currentRow);
-					currentMin = pqMin.getKey();
-					currentSeam[currentRow++] = currentPixel.getColumn(); 
-					
-					if(currentMin < min){
-						min = currentMin;
-						seam = currentSeam;
-					}
-					
-					break;
-				}
-				
-				seam[currentRow++] = currentPixel.getColumn();
-				
-				for(Edge<Boolean> e : energyGraph.outgoingEdges(currentV)){
-					Vertex<Integer> nextV = energyGraph.opposite(currentV, e);
-					int lengthToNext = currentV.getElement() + nextV.getElement();
-					pq.insert(lengthToNext, nextV);
-				}
-				
-				currentMin = pqMin.getKey();
-			}
-		}
-
-		return seam;
+		return new int[]{2,3,2};
 	}
 	
 	// Remove horizontal seam for current picture
@@ -202,61 +167,6 @@ public class SeamCarver {
 		if(seam == null)
 			throw new NullPointerException("Null argument given");
 		
-	}
-	
-	private class Pixel {
-		private int column;
-		private int row; 
-		private int hash = 0;
-		
-		public Pixel(int col, int row){
-			this.column = col;
-			this.row = row;
-		}
-		
-		public int getColumn(){
-			return column;
-		}
-		
-		public int getRow(){
-			return row;
-		}
-		
-		@Override
-		public int hashCode(){
-			if(hash != 0){
-				return hash;
-			} else{
-				hash = 17;
-				hash = 31 * hash + Integer.hashCode(column);
-				hash = 31 * hash + Integer.hashCode(row);
-			}
-			
-			return hash;
-		}
-		
-		@Override
-		public boolean equals(Object o){
-			
-			// performance optimization 
-			
-			// self check
-			if (this == o){
-				return true;
-			}
-			
-			// null check
-			if (o == null)
-				return false;
-			
-			if (getClass() != o.getClass()){
-				return false;
-			}
-			
-			Pixel p = (Pixel) o;
-			// field comparison
-			return ((column == p.column) && (row == p.row));
-		}
 	}
 	
 	
@@ -279,7 +189,7 @@ public class SeamCarver {
 	}
 	
 	public static void main(String[] args){
-		Picture p = new Picture("porsche.jpg");
+		Picture p = new Picture("6x5.png");
 		SeamCarver test = new SeamCarver(p);
 		int[] testSeam = test.findVerticalSeam();
 		System.out.println(testSeam);
