80baa547e891d3187c336248384ba0e2b5fd7890
Javier Torrez <javierctorrez@gmail.com>
2017-10-30 23:12:35 -0400

    finished implementing seam carving GUI

--- src/SeamCarverGUI.java
+++ src/SeamCarverGUI.java
@@ -1,84 +1,102 @@
 import javax.swing.*;
 
 import java.awt.BorderLayout;
-import java.awt.Image;
+import java.awt.GridLayout;
 import java.awt.event.*;
 
-public class SeamCarverGUI extends JFrame{
-	ImageIcon img;
-	JLabel imgHolder;
-	SeamCarver seamCarver;
-	int currentHeight; 
-	int currentWidth;
+public class SeamCarverGUI extends JFrame implements ActionListener{
+	private int height;
+	private int width;
+	
+	private ImageIcon img;
+	private JLabel imgContainer;
+	private JButton carveSeamBtn;
+	private JTextField seamCountField;
+	private JRadioButton horizontalSeamBtn, verticalSeamBtn;
+	private SeamCarver seamCarver;
 	
 	public SeamCarverGUI(String filename){
 		img = new ImageIcon(filename);
 		seamCarver = new SeamCarver(new Picture(filename));
-		currentHeight = img.getIconHeight();
-		currentWidth = img.getIconWidth();
+		
+		height = img.getIconHeight();
+		width = img.getIconWidth();
 		
 		initGUI();
+		
+		setTitle("Seam Carving");
+		setSize(width, height);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
 	public void initGUI(){
 		JPanel cp = (JPanel) this.getContentPane();
-		cp.addComponentListener(new resizeListener());
 		cp.setLayout(new BorderLayout());
+
+		JPanel optionsPanel = new JPanel(new BorderLayout());
+		JPanel radioPanel = new JPanel(new GridLayout(2, 1));
+		JPanel textPanel = new JPanel();
 		
-		imgHolder = new JLabel(img);
-		cp.add(imgHolder);
+		// ----- Options Panel ----- 
 		
-		setTitle("Seam Carving");
-		pack();
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		// Radio Button Panel
+		horizontalSeamBtn = new JRadioButton("Horizontal Seam");
+		
+		verticalSeamBtn = new JRadioButton("Vertical Seam");
+		verticalSeamBtn.setSelected(true);
+		
+		ButtonGroup radioBtnGroup = new ButtonGroup();
+		radioBtnGroup.add(horizontalSeamBtn);
+		radioBtnGroup.add(verticalSeamBtn);
+		
+		radioPanel.add(horizontalSeamBtn);
+		radioPanel.add(verticalSeamBtn);
+		
+		// Text Panel
+		JLabel pixelLabel = new JLabel("Pixels to remove: ");
+		seamCountField = new JTextField(10);
+		textPanel.add(pixelLabel);
+		textPanel.add(seamCountField);
+		textPanel.setBorder(BorderFactory.createEmptyBorder(10,0,0,0));
+		
+		// Seam Carve Button
+		carveSeamBtn = new JButton("Begin carving");
+		carveSeamBtn.addActionListener(this);
+
+		optionsPanel.add(radioPanel, BorderLayout.WEST);
+		optionsPanel.add(textPanel, BorderLayout.CENTER);
+		optionsPanel.add(carveSeamBtn, BorderLayout.EAST);
+		// --------------------------
+		
+		imgContainer = new JLabel(img);
+		cp.add(imgContainer, BorderLayout.CENTER);
+		cp.add(optionsPanel, BorderLayout.NORTH);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		int seamsCount = Integer.parseInt(seamCountField.getText());
+		boolean removeVerticalSeam = verticalSeamBtn.isSelected();
+		boolean removeHorizontalSeam = horizontalSeamBtn.isSelected();
+		
+		if(removeVerticalSeam){
+			for(int i = 0; i < seamsCount; i++){
+				seamCarver.removeVerticalSeam(seamCarver.findVerticalSeam());
+			}
+		} else if(removeHorizontalSeam){
+			for(int i = 0; i < seamsCount; i++){
+				seamCarver.removeHorizontalSeam(seamCarver.findHorizontalSeam());
+			}
+		}
+		
+		Picture seamImg = seamCarver.picture();
+		ImageIcon seamIcon = (ImageIcon) (seamImg.getJLabel().getIcon());
+		imgContainer.setIcon(seamIcon);
 	}
 	
 	public static void main(String[] args){
 		String imagePath = "chameleon.png";
 		SeamCarverGUI seamWindow = new SeamCarverGUI(imagePath);
 		seamWindow.setVisible(true);
-	}
-	
-	private void updateHeight(int h, int w){
-		this.currentHeight = h;
-		this.currentWidth = w;
-	}
-	
-	
-	private class resizeListener extends ComponentAdapter{
-		
-		@Override
-		public void componentResized(ComponentEvent evt){
-			int oldHeight = currentHeight;
-			int oldWidth = currentWidth;
-			
-			int newHeight = getHeight();
-			int newWidth = getWidth();
-			
-			Image scaled;
-			
-			boolean isHorizontalDecrease = (oldWidth > newWidth);
-			boolean isHorizontalIncrease = (oldWidth < newWidth);
-			boolean isVerticalDecrease = (oldHeight > newHeight);
-			boolean isVerticalIncrease = (oldHeight < newHeight);
-			
-			if(isHorizontalDecrease){
-				System.out.println("Horizontal Decrease");
-				seamCarver.removeVerticalSeam(seamCarver.findVerticalSeam());
-				Picture seamImg = seamCarver.picture();
-				ImageIcon seamIcon = (ImageIcon) (seamImg.getJLabel().getIcon());
-				imgHolder.setIcon(seamIcon);
-			} else if(isVerticalDecrease){
-				System.out.println("Vertical Decrease");
-				seamCarver.removeHorizontalSeam(seamCarver.findHorizontalSeam());
-				Picture seamImg = seamCarver.picture();
-				ImageIcon seamIcon = (ImageIcon) (seamImg.getJLabel().getIcon());
-				imgHolder.setIcon(seamIcon);
-			} else{
-				System.out.println("Nothing");
-			}
-			
-			updateHeight(newHeight, newWidth);
-		}
 	}
 }
